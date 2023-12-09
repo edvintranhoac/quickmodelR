34e30b10797d2ac8c8e909a514c9d21a8fe5f57b
Edvin Tran Hoac <etranhoac@wesleyan.edu>
2023-12-09 14:43:28 -0500

    small change

--- R/quickmodel.R
+++ R/quickmodel.R
@@ -8,7 +8,7 @@ quickmodel <- function(formula,
                        tuneGrid = NULL,
                        tuneLength = 3,
                        partition = 0.8,
-                       seed=1234,
+                       seed = 1234,
                        ...
                        ) {
 
